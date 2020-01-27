5f5b9f2fd48d7b6a50873c515a14b27007fa41e3
Jan Mojzis <jan.mojzis@gmail.com>
2020-01-27 15:42:55 +0100

    endiantest add

---
+++ Makefile
@@ -0,0 +1,14 @@
+CC=gcc
+CFLAGS+=-O3 -march=native -mtune=native -fwrapv -Wall
+
+all:  endiantest
+
+endiantest.o: endiantest.c
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c endiantest.c
+
+endiantest: endiantest.o 
+	$(CC) $(CFLAGS) $(CPPFLAGS) -o endiantest endiantest.o  $(LDFLAGS)
+
+clean:
+	rm -f *.o  endiantest
+
---
+++ endiantest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+static void hprint(unsigned char *h, long long hlen) {
+    long long i;
+    for (i = 0; i < hlen; ++i)
+        fprintf(stderr, "%02x", 255 & (int)h[i]);
+    fprintf(stderr, "\n");
+    fflush(stderr);
+}
+
+int main(int argc, char **argv) {
+
+    unsigned char x[4] = {1,2,3,4};
+
+    y = *(uint32_t *)x;
+
+    printf("%u\n", y);
+}
