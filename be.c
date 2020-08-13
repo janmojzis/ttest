85d8fb65919aa7d75a32ebead66e6b752cd7ee46
Jan Mojžíš <jan.mojzis@gmail.com>
2020-08-13 14:21:07 +0200

    be5

--- be.c
+++ be.c
@@ -11,10 +11,10 @@ typedef uint32_t vec32 __attribute__ ((vector_size (16)));
 #else
 vec32 vec32_beswap(vec32 u) {
     vec32 r;
-    r[0] = __builtin_bswap32(u[3]);
-    r[1] = __builtin_bswap32(u[2]);
-    r[2] = __builtin_bswap32(u[1]);
-    r[3] = __builtin_bswap32(u[0]);
+    r[0] = __builtin_bswap32(u[0]);
+    r[1] = __builtin_bswap32(u[1]);
+    r[2] = __builtin_bswap32(u[2]);
+    r[3] = __builtin_bswap32(u[3]);
     return r;
 }
 #endif
