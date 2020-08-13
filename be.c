8b565a94f69329cef5a4491f934caeb94c69224d
Jan Mojžíš <jan.mojzis@gmail.com>
2020-08-13 13:59:38 +0200

    be

---
+++ be.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdio.h>
+
+/* clang-format off */
+
+typedef uint32_t vec32 __attribute__ ((vector_size (16)));
+
+/* endianness */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define vec32_beswap(x) (x)
+#else
+vec32 vec32_beswap(vec32 u) {
+    vec r;
+    r[0] = __builtin_bswap32(u[0]);
+    r[1] = __builtin_bswap32(u[1]);
+    r[2] = __builtin_bswap32(u[2]);
+    r[3] = __builtin_bswap32(u[3]);
+    return r;
+}
+#endif
+
+int main() {
+
+    char x[16] = {0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15};
+    vec32 r = vec32_beswap(*(vec32 *)x);
+    vec32 z = {0, 1, 2, 4};
+    z = vec32_beswap(z);
+
+    printf("%u,%u,%u,%u\n", r[0], r[1], r[2], r[3]);
+    printf("%u,%u,%u,%u\n", z[0], z[1], z[2], z[3]);
+}
+
+
+
--- do
+++ do
@@ -75,3 +75,4 @@ echo "========================================================="
 #XXX
 sed s'/#include "crypto_stream.h"//' crypto_stream/chacha20/mojzis/little/crypto_stream_chacha20.c > x.c
 gcc -O3 -Wall -c x.c
+
